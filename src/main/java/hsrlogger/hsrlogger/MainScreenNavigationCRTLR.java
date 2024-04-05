2b607d7c5cc40eecf60cf145360ae31c345649ac
HanazonoArchive <157018362+HanazonoArchive@users.noreply.github.com>
2024-04-06 00:39:02 +0800

    Refine

--- src/main/java/hsrlogger/hsrlogger/MainScreenNavigationCRTLR.java
+++ src/main/java/hsrlogger/hsrlogger/MainScreenNavigationCRTLR.java
@@ -17,9 +17,13 @@ public class MainScreenNavigationCRTLR {
     @FXML
     private HBox MainBT_TeamBuilding;
 
+    @FXML
+    private HBox MainBT_RelicScore;
+
     private AnchorPane Main_ANCHRPNE;
     private boolean isPityCounterClicked = false;
     private  boolean isTeamBuildingClicked = false;
+    private  boolean isRelicScoreCliekd = false;
 
     public void Main_ANCHRPNE(AnchorPane Main_ANCHRPNE) {
         this.Main_ANCHRPNE = Main_ANCHRPNE;
@@ -43,7 +47,9 @@ public class MainScreenNavigationCRTLR {
                 isPityCounterClicked = !isPityCounterClicked;
                 MainBT_PityCounter.getStyleClass().setAll("Navigation_Tab", isPityCounterClicked ? "clicked" : "");
                 isTeamBuildingClicked = false;
+                isRelicScoreCliekd = false;
                 MainBT_TeamBuilding.getStyleClass().remove("clicked");
+                MainBT_RelicScore.getStyleClass().remove("clicked");
 
             } catch (Exception e){
                 e.printStackTrace();
@@ -64,8 +70,34 @@ public class MainScreenNavigationCRTLR {
                 isTeamBuildingClicked = !isTeamBuildingClicked;
                 MainBT_TeamBuilding.getStyleClass().setAll("Navigation_Tab", isTeamBuildingClicked ? "clicked" : "");
                 isPityCounterClicked = false;
+                isRelicScoreCliekd = false;
                 MainBT_PityCounter.getStyleClass().remove("clicked");
+                MainBT_RelicScore.getStyleClass().remove("clicked");
+
             } catch (Exception e){
+                e.printStackTrace();
+            }
+        } else if (event.getSource() == MainBT_RelicScore){
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreenUI_RelicScore.fxml"));
+                AnchorPane setMain_ANCHRPNE = loader.load();
+
+                MainScreenRelicScoreCRTLR controller = loader.getController();
+                controller.setMain_ANCHRPNE(Main_ANCHRPNE);
+
+                Main_ANCHRPNE.getChildren().remove(Main_ANCHRPNE);
+                Main_ANCHRPNE.getChildren().add(setMain_ANCHRPNE);
+
+                Main_ANCHRPNE = setMain_ANCHRPNE;
+
+                isRelicScoreCliekd = !isRelicScoreCliekd;
+                MainBT_RelicScore.getStyleClass().setAll("Navigation_Tab", isRelicScoreCliekd ? "clicked" : "");
+                isPityCounterClicked = false;
+                isTeamBuildingClicked = false;
+                MainBT_PityCounter.getStyleClass().remove("clicked");
+                MainBT_TeamBuilding.getStyleClass().remove("clicked");
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
---
+++ src/main/java/hsrlogger/hsrlogger/MainScreenRelicScoreCRTLR.java
@@ -0,0 +1,14 @@
+package hsrlogger.hsrlogger;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainScreenRelicScoreCRTLR {
+
+    @FXML
+    private AnchorPane Main_ANCHRPNE;
+
+    public void setMain_ANCHRPNE(AnchorPane Main_ANCHRPNE){
+        this.Main_ANCHRPNE = Main_ANCHRPNE;
+    }
+}
